dc7004cdcf320822730be3c00707e9df358e939a
Mahesh Chary <imciiit@gmail.com>
2022-02-15 12:09:13 +0530

    Added Multithreading...

--- src/main/java/com/publisher/root/controller/ProducerController.java
+++ src/main/java/com/publisher/root/controller/ProducerController.java
@@ -1,8 +1,14 @@
 package com.publisher.root.controller;
 
+
 import com.publisher.root.producer.Producer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import java.util.HashMap;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/publish")
@@ -10,14 +16,53 @@ public class ProducerController {
     @Autowired
     private Producer producer;
 
-    @PostMapping("/{message}")
-    public String publishMessage(@PathVariable("message")  String message){
-        try {
-            this.producer.sendMessage(message);
-            return  "Published.";
+    public  volatile  HashMap<String, Object> hm = new HashMap<>();
+
+    @GetMapping("/{message}")
+    public void publishMessage(@PathVariable("message")  String message){
+
+        try
+        {
+            CompletableFuture.runAsync(()->{
+                RestTemplate restTemplate = new RestTemplate();
+                synchronized (hm) {
+                    try {
+                        ResponseEntity<HashMap> responseData = restTemplate.exchange("https://reqres.in/api/users/3", HttpMethod.GET, null, HashMap.class);
+                       // Response response = new ObjectMapper().convertValue(responseData.getBody().get("data"), Response.class);
+                        hm.put("Response", responseData.getBody().get("data"));
+                        System.out.println(hm);
+                        hm.notify();
+                        System.out.println("API Thread Release lock on HashMap object");
+                    }
+                    catch (Exception ex){
+                        ex.printStackTrace();
+                    }
+                }
+            });
         }
         catch (Exception ex){
-            return ex.getMessage();
+            System.out.println(ex.getMessage());
+        }
+
+        try {
+            CompletableFuture.runAsync(() -> {
+                synchronized (hm) {
+                    System.out.println("Kafka Thread Started..");
+
+                    try {
+                        System.out.println("Kafka Thread waiting...");
+                        hm.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("Kafka Thread Resumed and Producing Message.");
+                    this.producer.sendMessage(hm);
+                }
+            });
+
+        }
+        catch (Exception ex){
+
         }
 
     }
--- src/main/java/com/publisher/root/producer/Producer.java
+++ src/main/java/com/publisher/root/producer/Producer.java
@@ -1,23 +1,32 @@
 package com.publisher.root.producer;
 
+import com.publisher.root.model.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+
 @Service
 public class Producer {
 
     private static final Logger log = LoggerFactory.getLogger(Producer.class);
-    private static final String topic = "test";
+    private static final String topic = "navyatest";
 
     @Autowired
-    private KafkaTemplate<String,String> kafkaTemplate;
-
-    public  void sendMessage(String message){
+    private KafkaTemplate<String,Object> kafkaTemplate;
+    //HashMap<String,Object> message
+    public  void sendMessage(HashMap<String,Object> message){
         log.info("Producing Message : {}",message);
-        this.kafkaTemplate.send(topic,message);
+        try {
+            this.kafkaTemplate.send(topic,message.toString());
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+        }
+
 
     }
 }
